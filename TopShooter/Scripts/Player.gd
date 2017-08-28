7f3f170fff4c1cce38deef8de54e63d78dd8882a
Gabriel Massadas <G4brym@users.noreply.github.com>
2017-08-28 01:24:37 +0100

    Now working with godot 2.2 unreleased version
    For multiplayer proposes

--- TopShooter/Scripts/Player.gd
+++ TopShooter/Scripts/Player.gd
@@ -11,65 +11,14 @@ export var WalkSpeed = 100
 ######################################################################################
 # Main functions
 
-var client1 = null
-var client2 = null
-var peer1 = null
-var peer2 = null
-var server = null
-
 func _ready():
-	var address = GDNetAddress.new()
-	address.set_host("localhost")
-	address.set_port(3000)
-
-	server = GDNetHost.new()
-	server.bind(address)
-
-	client1 = GDNetHost.new()
-	client1.bind()
-	peer1 = client1.connect(address)
-
-	client2 = GDNetHost.new()
-	client2.bind()
-	peer2 = client2.connect(address)
+	var host = NetworkedMultiplayerENet.new()
+	
 	set_process(true)
 	set_fixed_process(true)
 	
 
 func _process(delta):
-	if (client1.is_event_available()):
-		var event = client1.get_event()
-		
-		if (event.get_event_type() == GDNetEvent.CONNECT):
-			print("Client1 connected")
-			peer1.send_var("Hello from client 1", 0)
-			
-		if (event.get_event_type() == GDNetEvent.RECEIVE):
-			print(event.get_var())
-
-	if (client2.is_event_available()):
-		var event = client2.get_event()
-		
-		if (event.get_event_type() == GDNetEvent.CONNECT):
-			print("Client2 connected")
-			peer2.send_var("Hello from client 2", 0)
-			
-		if (event.get_event_type() == GDNetEvent.RECEIVE):
-			print(event.get_var())
-
-	if (server.is_event_available()):
-		var event = server.get_event()
-		
-		if (event.get_event_type() == GDNetEvent.CONNECT):
-			var peer = server.get_peer(event.get_peer_id())
-			var address = peer.get_address();
-			print("Peer connected from ", address.get_host(), ":", address.get_port())
-			peer.send_var(str("Hello from server to peer ", event.get_peer_id()), 0)
-			
-		elif (event.get_event_type() == GDNetEvent.RECEIVE):
-			print(event.get_var())
-			server.broadcast_var("Server broadcast", 0)
-	
 	pass
 	
 func _fixed_process(delta):
